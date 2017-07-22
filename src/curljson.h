55b11952b90d2e895d69a9f9a325d52b7e4c305c
USAGI <hogouxian@gmail.com>
2017-07-22 11:34:22 +0800

    created curlTest project

---
+++ src/curljson.h
@@ -0,0 +1,5 @@
+#ifndef _CURLJSON_H
+#define _CURLJSON_H
+
+
+#endif // _CURLJSON_H
---
+++ test/test_curljson.c
@@ -0,0 +1,15 @@
+#include "unity.h"
+#include "curljson.h"
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_curljson_NeedToImplement(void)
+{
+    TEST_IGNORE_MESSAGE("Need to Implement curljson");
+}
